bf9c0fd26d7b6c5773e42a43fbffbf6105ac28ae
Taylor-McNeil <mcneiltaylor@live.com>
2019-01-11 18:40:04 -0500

    Update LeetCode_TwoSum.java

--- LeetCode_TwoSum.java
+++ LeetCode_TwoSum.java
@@ -1,12 +1,5 @@
-import java.util.Arrays;
 import java.math.*;
 
-public class LeetCode_TwoSum {
-    public static void main(String[]args){
-        int [] a = {3,2,4};
-
-        System.out.println(Arrays.toString(twoSum(a,6)));
-    }
     public static int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++)
