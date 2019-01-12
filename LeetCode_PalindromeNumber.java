efe502fedf67371870dc67970ccfe357b74190a7
Taylor-McNeil <mcneiltaylor@live.com>
2019-01-12 17:32:16 -0500

    Create LeetCode_PalindromeNumber.java

---
+++ LeetCode_PalindromeNumber.java
@@ -0,0 +1,19 @@
+/*
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+Note in this situation -121 backward is 121- and that is not a palindrome.
+*/
+
+public boolean isPalindrome(int x) {
+        String number = Integer.toString(x);
+        int length = number.length();
+        
+        StringBuilder sb = new StringBuilder();
+        while(length!=0){
+            sb.append(number.charAt(length-1));
+            length--;
+        }
+        String reversed = sb.toString();
+        if(number.equals(reversed)) return true;
+         
+        return false;
+    }
