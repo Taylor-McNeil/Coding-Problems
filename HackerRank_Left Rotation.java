bd286a0ed25c8877b78b1909a8a75586d06702e5
Taylor-McNeil <mcneiltaylor@live.com>
2019-09-19 15:16:32 -0400

    Create HackerRank_Left Rotation.java

---
+++ HackerRank_Left Rotation.java
@@ -0,0 +1,10 @@
+ StringBuilder sb = new StringBuilder();   
+     for(int i=d;i<a.length;i++){
+         sb.append(a[i]+" ");
+     }
+        for(int i=0;i<d;i++){
+            sb.append(a[i]+" ");
+        }
+        
+        System.out.println(sb.toString());
+        
